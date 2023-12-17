e5d15f4e2dabce2a69ce1304c80a573af7ceb006
MathieuTrachino <82705419+Matraque@users.noreply.github.com>
2023-12-17 12:42:36 +0100

    Update app.en.ts
    welcome to

--- i18n/lang/app.en.ts
+++ i18n/lang/app.en.ts
@@ -1,6 +1,6 @@
 const translation = {
   common: {
-    welcome: 'Welcome to use',
+    welcome: 'Welcome to',
     appUnavailable: 'App is unavailable',
     appUnkonwError: 'App is unavailable',
   },
