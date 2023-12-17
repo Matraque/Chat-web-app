3862f5d8d051485521ed872b14e5368537998361
MathieuTrachino <82705419+Matraque@users.noreply.github.com>
2023-12-17 11:02:37 +0000

    env.AI_APP

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: '${process.env.AI_APP}',
+  title: `${process.env.AI_APP}`,
   description: 'AI app description',
   copyright: 'GenDojo.ai',
   privacy_policy: '',
